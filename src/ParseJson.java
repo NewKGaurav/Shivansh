635075406034f52c6c4e3b0aaca359f4d391e42d
NewKGaurav <gauravsingh.08sv@gmail.com>
2023-05-08 12:58:34 +0530

    Fourth Commit

---
+++ src/ParseJson.java
@@ -0,0 +1,14 @@
+package ParseJson;
+import io.restassured.path.json.JsonPath;
+
+public class ParseJson {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ JsonPath js=new JsonPath(payload.CoursePrice());
+ System.out.println(js.getInt("courses.size"));
+ System.out.println(js.get("dashboard.purchaseamount").toString());
+ System.out.println(js.get("courses[0].title").toString());
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module DummyAPI {
+}
